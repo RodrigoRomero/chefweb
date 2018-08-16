3f034106192d189be60abfdeb38a07bdc4ffd657
Rodrigo Romero <construirweb@hotmail.com>
2018-08-16 17:12:46 -0300

    como comprar quienes somos

---
+++ application/views/institucional/como-comprar.php
@@ -0,0 +1,40 @@
+<div class="col-md-3">
+	<div class="feature-box fbox-plain">
+		<div class="fbox-icon">
+			<a href="#"><i class="i-alt">1.</i></a>
+		</div>
+		<h3>Elegir los productos</h3>
+		<p>Perferendis, nam. Eum aperiam vel animi beatae corporis dignissimos, molestias, placeat, maxime optio ipsam nostrum atque quidem.</p>
+	</div>
+	[VIDEO YOUTUBE]
+</div>
+<div class="col-md-3">
+	<div class="feature-box fbox-plain">
+		<div class="fbox-icon">
+			<a href="#"><i class="i-alt">2.</i></a>
+		</div>
+		<h3>Registrate o Logueate</h3>
+		<p>Perferendis, nam. Eum aperiam vel animi beatae corporis dignissimos, molestias, placeat, maxime optio ipsam nostrum atque quidem.</p>
+	</div>
+	[VIDEO YOUTUBE]
+</div>
+<div class="col-md-3">
+	<div class="feature-box fbox-plain">
+		<div class="fbox-icon">
+			<a href="#"><i class="i-alt">3.</i></a>
+		</div>
+		<h3>Confirma tu pedido</h3>
+		<p>Perferendis, nam. Eum aperiam vel animi beatae corporis dignissimos, molestias, placeat, maxime optio ipsam nostrum atque quidem.</p>
+	</div>
+	[VIDEO YOUTUBE]
+</div>
+<div class="col-md-3">
+	<div class="feature-box fbox-plain">
+		<div class="fbox-icon">
+			<a href="#"><i class="i-alt">4.</i></a>
+		</div>
+		<h3>Confirmación Telefónica/Email</h3>
+		<p>Perferendis, nam. Eum aperiam vel animi beatae corporis dignissimos, molestias, placeat, maxime optio ipsam nostrum atque quidem.</p>
+	</div>
+	[VIDEO YOUTUBE] ???
+</div>
---
+++ application/views/institucional/quienes-somos.php
@@ -0,0 +1,23 @@
+
+
+<div class="container">
+  <?php //echo up_asset('about-me/rodrigo_romero_about.jpg',['class'=>'img-responsive', 'alt'=>'Rodrigo Romero', "title"=>"Rodrigo Romero"]); ?>
+  <!--======= WHO WE ARE =========-->
+  <div class="who-we">
+
+            <p>
+            - Y... Qué hacemos? (Es la pregunta recurrente entre mis amigos)<br>
+            <b>- Juntémonos a comer!</b> (La respuesta que con el paso del tiempo se tornaría en el común denominador)<br><br>
+            A través de esta propuesta comienza para mí un viaje magnífico y embriagador.<br>
+            La idea de brindarles una experiencia nueva en cada encuentro conciliando sus gustos en un menú, es el puntapie inicial para el aprendizaje y la investigación.<br>
+            Gracias al apoyo de mis amigos, que se ofrecen comida tras comida como mis "conejillos de indias", juego y me divierto experimentando distintos <b>tipos de cocción, texturas, sabores, colores y aromas</b>, desde diversos cortes de carnes y verduras a la parrilla hasta un chivo al asador; intentar hacerlos viajar hacia el Noroeste con unos típicos tamales Argentinos o a vivir una noche en Japón a través del Ramen.<br>
+            Así es que esta forma de cocinar libre y con espíritu hippie sembró, como bien lo describe Eduardo Galeano en su Libro de los Abrazos, un fueguito que empieza a brillar con luz propia, despertando en mí el deseo de emprender un nuevo viaje, formándome como Profesional Gastronómico en el IAG.<br>
+            Cada día estoy más convencido de que este es el camino.
+            </p>
+
+
+  <?php echo up_asset('about-me/20.png',['class'=>'img-responsive', 'alt'=>'Rodrigo Romero', "title"=>"Rodrigo Romero"]); ?>
+
+  </div>
+
+</div>
