12176a9520a21407055e63de067787471801f839
Rodrigo Romero <construirweb@hotmail.com>
2018-08-17 15:39:44 -0300

    banner home

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -1,20 +1,20 @@
 <div class="container clearfix">
 
 	<div class="col-md-8 nopadding">
+		<div class="col-md-12 noleftpadding bottommargin-sm">
+			<?php echo up_asset('slider/b1.jpg') ?>
+		</div>
+		<div class="clear"></div>
 
-		<div class="col-md-6 noleftpadding bottommargin-sm">
+		<div class="col-md-6 noleftpadding">
 			<a href="#"><img src="images/shop/banners/2.jpg" alt="Image"></a>
 		</div>
 
-		<div class="col-md-6 noleftpadding bottommargin-sm">
+		<div class="col-md-6 noleftpadding">
 			<a href="#"><img src="images/shop/banners/8.jpg" alt="Image"></a>
 		</div>
 
-		<div class="clear"></div>
 
-		<div class="col-md-12 noleftpadding">
-			<a href="#"><img src="images/shop/banners/4.jpg" alt="Image"></a>
-		</div>
 
 	</div>
 
