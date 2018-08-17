64468c032b8f44cc7d85188e8524de0749a04ba0
Rodrigo Romero <construirweb@hotmail.com>
2018-08-17 15:05:28 -0300

    como comprar

--- application/views/institucional/como-comprar.php
+++ application/views/institucional/como-comprar.php
@@ -4,9 +4,8 @@
 			<a href="#"><i class="i-alt">1.</i></a>
 		</div>
 		<h3>Elegir los productos</h3>
-		<p>Perferendis, nam. Eum aperiam vel animi beatae corporis dignissimos, molestias, placeat, maxime optio ipsam nostrum atque quidem.</p>
+		<p>Selecciona todos los productos que desees, y añadelos al carrito.<br/>Recuerda que vienen en presentaciones de 6 unidades.</p>
 	</div>
-	[VIDEO YOUTUBE]
 </div>
 <div class="col-md-3">
 	<div class="feature-box fbox-plain">
@@ -14,9 +13,8 @@
 			<a href="#"><i class="i-alt">2.</i></a>
 		</div>
 		<h3>Registrate o Logueate</h3>
-		<p>Perferendis, nam. Eum aperiam vel animi beatae corporis dignissimos, molestias, placeat, maxime optio ipsam nostrum atque quidem.</p>
+		<p>Una vez que tienes todo lo que deseas en el carrito, el sistema automáticamente te pedirá que te loguees o bien que crees tu cuenta.<br/>Si ya tienes cuenta y conoces bien como es el proceso, incluso puedes loguearte antes de iniciar tu pedido.</p>
 	</div>
-	[VIDEO YOUTUBE]
 </div>
 <div class="col-md-3">
 	<div class="feature-box fbox-plain">
@@ -24,9 +22,8 @@
 			<a href="#"><i class="i-alt">3.</i></a>
 		</div>
 		<h3>Confirma tu pedido</h3>
-		<p>Perferendis, nam. Eum aperiam vel animi beatae corporis dignissimos, molestias, placeat, maxime optio ipsam nostrum atque quidem.</p>
+		<p>Una vez que tengas todos tus productos en el carrito, deberás confirmar el pedido. El sistema automáticamente te enviará vía mail una copia de lo que has pedido para que tengas constancia del mismo.</p>
 	</div>
-	[VIDEO YOUTUBE]
 </div>
 <div class="col-md-3">
 	<div class="feature-box fbox-plain">
@@ -34,7 +31,16 @@
 			<a href="#"><i class="i-alt">4.</i></a>
 		</div>
 		<h3>Confirmación Telefónica/Email</h3>
-		<p>Perferendis, nam. Eum aperiam vel animi beatae corporis dignissimos, molestias, placeat, maxime optio ipsam nostrum atque quidem.</p>
+		<p>Una vez que recibamos tu pedido, y empecemos a procesar el mismo. Te estaremos contactando para ultimar los detelles del mismo y coordinar la mejor forma posible en la entrega del producto.</p>
 	</div>
-	[VIDEO YOUTUBE] ???
+</div>
+<div class="clear"></div>
+<div class="col-md-3">
+	<div class="feature-box fbox-plain">
+		<div class="fbox-icon">
+			<a href="#"><i class="i-alt">5.</i></a>
+		</div>
+		<h3>Seguimiento de mi pedido</h3>
+		<p>Desde la sección mi cuenta, podrás hacer el seguimiento de tu pedido y enterarte en que estado se encuentra el mismo.</p>
+	</div>
 </div>
