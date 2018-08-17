a05e06497be7b291adefcdf59dfff51d263fe5cf
Rodrigo Romero <construirweb@hotmail.com>
2018-08-17 17:01:05 -0300

    banner launch

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -1,25 +1,25 @@
 <div class="container clearfix">
 
 	<div class="col-md-8 nopadding">
-		<div class="col-md-12 noleftpadding bottommargin-sm">
+		<div class="col-md-12 col-sm-12 noleftpadding bottommargin-sm">
 			<?php echo up_asset('slider/b1.jpg') ?>
 		</div>
 		<div class="clear"></div>
 
-		<div class="col-md-6 noleftpadding">
-			<a href="#"><img src="images/shop/banners/2.jpg" alt="Image"></a>
+		<div class="col-md-6 col-sm-12 noleftpadding bottommargin-sm">
+			<a href="#"><img src="images/shop/banners/2.jpg" alt="Image" class="img-responsive"></a>
 		</div>
 
-		<div class="col-md-6 noleftpadding">
-			<a href="#"><img src="images/shop/banners/8.jpg" alt="Image"></a>
+		<div class="col-md-6 col-sm-12 noleftpadding bottommargin-sm">
+			<?php echo up_asset('slider/b3.jpg') ?>
 		</div>
 
 
 
 	</div>
 
-	<div class="col-md-4 nopadding">
-		<?php echo up_asset('slider/b2.jpg') ?>
+	<div class="col-md-4 col-sm-12 nopadding">
+		<a href="<?php echo base_url('/productos') ?>"><?php echo up_asset('slider/b2.jpg') ?></a>
 	</div>
 
 	<div class="clear"></div>
