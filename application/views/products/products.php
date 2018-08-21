a47f5133741255691461931c81f33cc852cea520
Rodrigo Romero <construirweb@hotmail.com>
2018-08-21 16:16:09 -0300

    seo schema

--- application/views/products/products.php
+++ application/views/products/products.php
@@ -8,10 +8,10 @@ foreach($productos as $product) {
 	$link = '/productos/'.url_title($product->nombre,'-',true).'/'.$product->id;
 
 ?>
-	<div class="product clearfix">
+	<div class="product clearfix" itemscope itemtype="http://schema.org/Product">
 		<div class="product-image">
 
-			<a href="<?php echo base_url($link); ?>" alt="<?php echo $product->nombre ?>" title="<?php echo $product->nombre ?>"><?php echo up_asset($file_name, ['alt' =>  $product->nombre, 'title'=> $product->nombre]); ?></a>
+			<a href="<?php echo base_url($link); ?>" alt="<?php echo $product->nombre ?>" title="<?php echo $product->nombre ?>"><?php echo up_asset($file_name, ['itemprop'=>'image', 'alt' =>  $product->nombre, 'title'=> $product->nombre]); ?></a>
 
 			<?php  if(!empty($product->precio_oferta) &&
 		    	      ($hoy < $timelimit) &&
@@ -24,16 +24,16 @@ foreach($productos as $product) {
 			</div>
 		</div>
 		<div class="product-desc">
-			<div class="product-title"><h4 class="nomargin"><a href="<?php echo base_url($link); ?>" ><?php echo $product->nombre ?></a></h4></div>
+			<div class="product-title"><h4 class="nomargin" itemprop="name"><a href="<?php echo base_url($link); ?>" ><?php echo $product->nombre ?></a></h4></div>
 
-			<div class="product-price">
-
+			<div class="product-price" itemscope itemtype="http://schema.org/Offer">
+				<meta itemprop="priceCurrency" content="ARS" />
 		    <?php  if(!empty($product->precio_oferta) &&
 		    	      ($hoy < $timelimit) &&
 		    	      $product->precio_regular > $product->precio_oferta) { ?>
     	      	<span class="price-unit">$</span><?php echo number_format($product->precio_oferta, 0, ",", ". ") ?> - <span class="price-tenure"><del>$ <?php echo number_format($product->precio_regular, 2, ",", ". ") ?></del></span>
 	   		<?php } else { ?>
-	   			 <span class="price-unit">$</span><?php echo number_format($product->precio_regular, 0, ",", ". ") ?>
+	   			 <span class="price-unit">$</span><span  itemprop="price"><?php echo number_format($product->precio_regular, 0, ",", ". ") ?></span>
 	   		<?php } ?>
 	   		</div>
 		</div>
