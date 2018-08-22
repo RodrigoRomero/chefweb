048e2167189194a57f186129031b44edc79fa92c
Rodrigo Romero <construirweb@hotmail.com>
2018-08-22 17:31:45 -0300

    schema comments

--- application/views/institucional/testimonios.php
+++ application/views/institucional/testimonios.php
@@ -1,33 +1,28 @@
+<?php
+
+$testimonios = [
+				['texto'=> 'Las burguers de garbanzos estan increíbles.', 'autor' => 'IA'],
+				['texto'=> 'Muy buenas las hamburguesas de lentejas!.', 'autor' => 'Geo'],
+				['texto'=> 'Las hamburguesas riquísimas !', 'autor' => 'Marcia'],
+				['texto'=> 'Muy buenas las hamburguesas. Probamos las de lentejas y las de garbanzos ya', 'autor' => 'Rodri']
+];
+?>
+
+
 <div class="fslider testimonial" data-animation="fade" data-arrows="false">
-	<div class="flexslider">
+	<div class="flexslider"  itemscope itemtype="http://schema.org/UserComments">
 		<div class="slider-wrap">
+			<?php foreach($testimonios as $testimonio) { ?>
 			<div class="slide">
-
 				<div class="testi-content">
-					<p>Las burguers de garbanzos estan increíbles</p>
-					<div class="testi-meta">
-						Ia
+					<p itemprop="commentText"><?php echo $testimonio['texto'] ?></p>
+					<div class="testi-meta" itemprop="creator">
+						<?php echo $testimonio['autor'] ?>
 					</div>
 				</div>
 			</div>
-			<div class="slide">
 
-				<div class="testi-content">
-					<p>Muy buenas las hamburguesas de lentejas!.</p>
-					<div class="testi-meta">
-						Geo
-					</div>
-				</div>
-			</div>
-			<div class="slide">
-
-				<div class="testi-content">
-					<p>Las hamburguesas riquísimas !</p>
-					<div class="testi-meta">
-						Marcia
-					</div>
-				</div>
-			</div>
+			<?php } ?>
 		</div>
 	</div>
 </div>
